a6fffa252ce55445d9a82d5a06a73d6215cc9b34
GaussNoob <koadgg15@gmail.com>
2026-03-18 14:36:17 -0300

    feat: add mason ensure_installed

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,1 +1,25 @@
-
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "csharp-language-server",
+        "omnisharp",
+        "netcoredbg",
+        "eslint-lsp",
+        "typescript-language-server",
+        "prettier",
+        "pyright",
+        "ruff",
+        "ruff-lsp",
+        "jdtls",
+        "java-debug-adapter",
+        "lua-language-server",
+        "stylua",
+        "shfmt",
+        "dockerfile-language-server",
+        "tree-sitter-cli",
+      },
+    },
+  },
+}
